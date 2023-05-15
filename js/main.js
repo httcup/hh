3ec5fc7286735a9fb2d0806c9b1a06116e53be60
boyhh <htt@163.com>
2023-05-15 08:39:52 +0000

    update js/main.js.
    Signed-off-by: boyhh <htt@163.com>

--- js/main.js
+++ js/main.js
@@ -383,16 +383,16 @@ function check() {
 var d = new Date();
 var time = d.getHours();
 if (time < 24) {
-    document.getElementById("title").innerHTML = "xhxh.vip|小胡小胡主页";
+    document.getElementById("title").innerHTML = "zhu.pw|主页";
 }
 if (time < 18) {
-    document.getElementById("title").innerHTML = "xhxh.vip|小胡小胡主页 ";
+    document.getElementById("title").innerHTML = "zhu.pw|主页 ";
 }
 if (time < 12) {
-    document.getElementById("title").innerHTML = "xhxh.vip|小胡小胡主页";
+    document.getElementById("title").innerHTML = "zhu.pw|主页";
 }
 if (time < 5) {
-    document.getElementById("title").innerHTML = "xhxh.vip|小胡小胡主页";
+    document.getElementById("title").innerHTML = "zhu.pw|主页";
 }
 //title问候语结束
 
